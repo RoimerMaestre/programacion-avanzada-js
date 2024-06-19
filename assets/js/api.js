0e9752ad00f49d0fc58cc15b08c3ee4247f50b93
RoimerMaestre <roimer.maestre93@gmail.com>
2024-06-19 11:51:30 -0400

    se agrega info nueva

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,6 +2,10 @@
 export const obtenerAnimales = async () => {
     try {
       const response = await fetch('assets/js/animales.json');
+      if (!response.ok) {
+        // Si no es correcta, lanzamos un error con un mensaje específico, es importante destacar que segun lo estudiado, trow new error es muy utilizado en los manejos de errores, y tengo entendido que es una buena practica
+        throw new Error('Hay un error en la llamada de la api ');
+      }
       const data = await response.json();
       
       return data.animales;
